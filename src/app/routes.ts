6d08c35b9c0beeb53d0a99ef2f5d389287a6afef
quickbow <richardroypalathinal@gmail.com>
2026-09-18 13:56:20 -0800

    Support routing on GitHub Pages

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -6,29 +6,32 @@ import { DeliveryView } from "./views/DeliveryView";
 import { Login } from "./components/Login";
 import { SignUp } from "./components/SignUp";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: LandingPage,
-  },
-  {
-    path: "/login",
-    Component: Login,
-  },
-  {
-    path: "/signup",
-    Component: SignUp,
-  },
-  {
-    path: "/customer",
-    Component: CustomerView,
-  },
-  {
-    path: "/seller",
-    Component: SellerView,
-  },
-  {
-    path: "/delivery",
-    Component: DeliveryView,
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      Component: LandingPage,
+    },
+    {
+      path: "/login",
+      Component: Login,
+    },
+    {
+      path: "/signup",
+      Component: SignUp,
+    },
+    {
+      path: "/customer",
+      Component: CustomerView,
+    },
+    {
+      path: "/seller",
+      Component: SellerView,
+    },
+    {
+      path: "/delivery",
+      Component: DeliveryView,
+    },
+  ],
+  { basename: import.meta.env.BASE_URL },
+);
